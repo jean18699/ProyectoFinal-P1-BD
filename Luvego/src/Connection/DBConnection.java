53b96f2d5ebc4d937c930d27dfc363cbaf723dc4
hardow2011 <hardow2011@outlook.com>
2019-11-29 18:52:32 -0400

    driver anadido y prueba de conex

--- Luvego/src/Connection/DBConnection.java
+++ Luvego/src/Connection/DBConnection.java
@@ -11,8 +11,18 @@ public class DBConnection {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		
-		
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
+        //Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=GroceryStore;instance=MSSQLSERVER;integratedSecurity=true");
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://ojosdelacara.database.windows.net:1433;database=Luvego;user=JeanGeorge@ojosdelacara;password=Luvego12*45JeanKGeorge;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
+
+        Statement sta = conn.createStatement();
+		String Sql = "SELECT * FROM TestTable";
+		ResultSet rs = sta.executeQuery(Sql);
+		while (rs.next()) {
+			System.out.println(rs.getString(1) + " " + rs.getString(2));
+		}
+			
 	}
 
 }
